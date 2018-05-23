b13b67dd3743e7672367282218b4e60c18abb3ca
arioscen <arioscen@gmail.com>
2018-05-23 07:56:44 -0700

    create foo

--- myproject/settings.py
+++ myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'foo.apps.FooConfig',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
