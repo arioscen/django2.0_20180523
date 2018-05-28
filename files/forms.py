0950ac5f45b204eaeae17d1016df8b8e92181f4b
ivan_weng <ivan_weng@tw.shuttle.com>
2018-05-28 16:22:07 +0800

    limit upload size

--- files/forms.py
+++ files/forms.py
@@ -4,6 +4,19 @@ from django import forms
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 
+    def clean_file(self):
+        file = self.cleaned_data["file"]
+        if file.size > 10:
+            raise forms.ValidationError("file too large (limit 10 byte)")
+        else:
+            return file
+
 
 class FileFieldForm(forms.Form):
     file_field = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+
+    def clean_file_field(self):
+        file_field = self.cleaned_data["file_field"]
+        if file_field.size > 100:
+            raise forms.ValidationError("files too large (limit 100 byte)")
+        return file_field
