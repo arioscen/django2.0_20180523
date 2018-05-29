c6347cc4d54f8a2100c3c271c802d52da01d39eb
ivan_weng <ivan_weng@tw.shuttle.com>
2018-05-29 09:00:20 +0800

    change upload limit

--- files/forms.py
+++ files/forms.py
@@ -6,7 +6,7 @@ class UploadFileForm(forms.Form):
 
     def clean_file(self):
         file = self.cleaned_data["file"]
-        if file.size > 10:
+        if file.size > 1024*1024:
             raise forms.ValidationError("file too large (limit 10 byte)")
         else:
             return file
@@ -17,6 +17,6 @@ class FileFieldForm(forms.Form):
 
     def clean_file_field(self):
         file_field = self.cleaned_data["file_field"]
-        if file_field.size > 100:
+        if file_field.size > 1024*1024:
             raise forms.ValidationError("files too large (limit 100 byte)")
         return file_field
